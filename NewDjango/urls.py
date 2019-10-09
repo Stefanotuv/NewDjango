9902ed1ae6d98e7afc8ded005e696e824395b3bc
stefanotuv <ste.tuveri@gmail.com>
2019-10-09 22:11:16 +0100

    users app

--- NewDjango/urls.py
+++ NewDjango/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path
 from test_app import views
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index, name = 'index'),
@@ -28,3 +30,6 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('users/', include('users.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
