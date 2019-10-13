d4ae6f0f994031bfee3dddea4697bb31d8fd8920
stefanotuv <ste.tuveri@gmail.com>
2019-10-14 00:24:11 +0100

    Added the admin custom for post

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from datetime import datetime
 
 # Create your models here.
 
@@ -21,5 +21,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def to_dictionary(self):
+        output = {
+            "title":self.title,
+            "content":self.content[:12],
+            "author":self.author.username,
+            "date_posted":self.date_posted.strftime("%d-%b-%Y (%H:%M:%S.%f)"),
+        }
+        return output
 
 
--- grouping/urls.py
+++ grouping/urls.py
@@ -1,10 +1,17 @@
 __author__ = "stefanotuv"
 
 from django.urls import path
-from .views import HomeGroupingView, TableAPIView,TableView
+from .views import HomeGroupingView, TableAPIView,TableView, TablePostsAPIView, TablePostsView
 
 urlpatterns = [
     path('', HomeGroupingView.as_view(), name='grouping-home'),
-    path('api/table/',TableAPIView.as_view(),name='grouping-api'),
-    path('tables/',TableView.as_view(),name='grouping-table')
+
+    path('api/table/',TableAPIView.as_view(),name='grouping-api-table'),
+    path('api/table-posts/', TablePostsAPIView.as_view(), name='grouping-api-table-posts'),
+
+    path('tables/',TableView.as_view(),name='grouping-table'),
+    path('tables-posts/',TablePostsView.as_view(),name='grouping-table-posts'),
+    path('simple-table/',TablePostsView.as_view(template_name="grouping/table_simple.html")),
+    path('simple-table-ajax/',TablePostsView.as_view(template_name="grouping/table_simple_ajax.html")),
+    path('dynamic-table-ajax/',TablePostsView.as_view(template_name="grouping/tables_dynamic_ajax.html"))
 ]
